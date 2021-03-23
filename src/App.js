02b0a48a8de72be457efed2062c9aa39067e52a6
EmilioHND <peedroemilio24@gmail.com>
2021-03-23 13:47:10 -0600

    Agregando Crud URL

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 //Css
 import './css/App.css'
 
+//Components
+import Links from './components/Links'
+import LinkForm from './components/LinkForm'
+
 function App() {
   return (
     <div className="App">
-      
+      <LinkForm />
+      <Links />
     </div>
   );
 }
---
+++ src/components/LinkForm.js
@@ -0,0 +1,22 @@
+import { Container, Form, Button } from "react-bootstrap";
+
+const LinkForm = () => {
+  return (
+    //Formulario
+    <Container className="border mt-3 p-5 rounded bg-primary">
+      <h2 className="text-white mt-2">CRUD URL</h2>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Control type="text" placeholder="http//:example.com" className="mt-3" />
+          <Form.Control type="text" placeholder="Nombre de la web" className="mt-3" />
+          <Form.Control as="textarea" rows={3} className="mt-3"/>
+        </Form.Group>
+      </Form>
+
+      <Button variant="success">Guardar</Button>
+      <Button className="ml-2" variant="danger">Eliminar</Button>
+      <Button className="ml-2" variant="secondary">Restaurar</Button>
+    </Container>
+  );
+};
+export default LinkForm;
